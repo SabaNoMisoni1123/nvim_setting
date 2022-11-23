4dd0d45772f2975b548aec9d07dba045ef0f9437
skondo_wsl_desctop <s.kondo11235813@gmail.com>
2022-11-23 17:46:46 +0900

    fuzzy finderを追加

--- luascripts/telescope.lua
+++ luascripts/telescope.lua
@@ -1,5 +1,5 @@
 -- default setting
-require("telescope").setup{
+require('telescope').setup {
   defaults = {
     mappings = {
       n = {
@@ -11,10 +11,24 @@ require("telescope").setup{
       },
     },
   },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    }
+  }
 }
+
+-- load extensions
+require('telescope').load_extension('fzf')
 
 -- keymap
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>fc', builtin.commands, {})
+vim.keymap.set('n', '<leader>fc', builtin.command_history, {})
+vim.keymap.set('n', '<leader>fs', builtin.spell_suggest, {})
+vim.keymap.set('n', '<leader>fC', builtin.commands, {})
+vim.keymap.set('n', '/', builtin.current_buffer_fuzzy_find, {})
